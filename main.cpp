5e1f90285723c7b37588ea252c6ce407ac5a6d83
Gajumaru <igm6323@gmail.com>
2026-03-11 01:28:03 +0900

    scanfをcinに変更 (423バイト)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
-#include <stdio.h>
+#include <iostream>
 int b[91],X=90,x,Y,t,a,i,j,f,p,d[]{-9,-8,1,10,9,8,-1,-10};void C(){a=0;for(i=b[
 X]-3?0:8;i--;){for(j=1;b[Y=X+d[i]*j]==!t;)j++;if(1<j&&b[Y]==t)for(;j--;a++)f?b[
 X+d[i]*j]=t:0;}}int main(){for(;X--;p=b[X]=X%9&&X>8&&X<81?X-40&&X-50?X-41&&X-49
 ?3:0:1:2);for(;p;x=f=0){for(X=7;X++<81;putchar("0@\n-"[b[X]]))C(),a?x=X:0;x?Y=0
-,t?X=x:scanf_s("%d%d",&Y,&X),f=X+=Y*9,C(),t=a?p=2,t:!t:(puts("PASS"),p--);t=!t;
-}}
+,t?X=x:(std::cin>>Y>>X,0),f=X+=Y*9,C(),t=a?p=2,t:!t:(puts("PASS"),p--);t=!t;}}
