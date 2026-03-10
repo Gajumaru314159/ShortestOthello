08bd0eca97cb9ffa0ae3344417cf50a853a0aaa5
Gajumaru <igm6323@gmail.com>
2026-03-11 02:24:59 +0900

    b[90]の初期化ミスによるセグフォを修正

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-b[91],X=90,x,Y,t,a,f,d;C(i,j){for(i=b[X]/3*9;i--;){for(d=i+i/3*6-10,j=1;b[Y=X+d
+b[91],X=91,x,Y,t,a,f,d;C(i,j){for(i=b[X]/3*9;i--;){for(d=i+i/3*6-10,j=1;b[Y=X+d
 *j++]==!t;);if(2<j&b[Y]==t)for(x=X;j--*f;a++)b[X+d*j]=t;}}main(p){for(;X--;b[X
 ]=X%9&&X>8&X<81?X-40&&X-50?X-41&&X-49?3:0:1:2);for(;p;x?t?X=x:scanf("%d%d",&Y,&
 X),f=X+=!t*Y*9,C(),p=!a?t=!t,p:2:puts("PASS")&p--,t=!t,x=f=0)for(X=7;81>X++;a=!
