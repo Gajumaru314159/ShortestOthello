2f18c7acfe1561e3d0d82d1253b5c40c94137b9c
Gajumaru <igm6323@gmail.com>
2026-03-11 01:29:55 +0900

    fの判定をforに埋め込み (390バイト)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 int b[91],X=90,x,Y,t,a,i,j,f,p,d;void C(){for(i=b[X]-3?0:9;i--;){for(d=i+i/3*6-
-10,j=1;b[Y=X+d*j++]==!t;);if(2<j&b[Y]==t)for(x=X;j--;a++)f?b[X+d*j]=t:0;}}int
+10,j=1;b[Y=X+d*j++]==!t;);if(2<j&b[Y]==t)for(x=X;j--&&f;a++)b[X+d*j]=t;}}int
 main(){for(;X--;p=b[X]=X%9&&X>8&X<81?X-40&&X-50?X-41&&X-49?3:0:1:2);for(;p;x=f=
 0){for(X=7;X++<81;a=!putchar("0@\n-"[b[X]]))C();x?Y=0,1?X=x:(std::cin>>Y>>X,0),
 f=X+=Y*9,C(),a?p=2:t=!t:puts("PASS")&p--;t=!t;}}
