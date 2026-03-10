fd9b95bdfabb6cb69c44836e41abd877158b3aa3
Gajumaru <igm6323@gmail.com>
2026-03-11 01:27:55 +0900

    d配列の初期化を変更 (427バイト)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
-int b[81],x,X,Y,t,a,c,i,j,f,p,d[]={-9,-8,1,10,9,8,-1,-10};int C(){a=0;if(b[X]==
-2)for(i=8;i--;){for(j=0,Y=X+d[i];b[Y%81]==!t;Y+=d[i])j++;if(j&&b[Y]==t)for(;j--
-+2;Y-=d[i],a++)f?b[Y]=t:0;}return a;}int main(){for(x=81;x--;b[x]=x%9-8&&x>8?2:
-3);b[39]=b[49]=0;p=t=b[40]=b[48]=1;for(;p+1;){x=f=0;for(X=7;X++<80;printf(" %c"
-,"O@-\n"[b[X]]))C()?x=X:0;if(x)Y=0,t?scanf("%d%d",&X,&Y):X=x+1,f=X+=Y*9-1,t=C()
-?t:!t;else puts("pass"),p--;t=!t;}return 0;}
+int b[91],X=90,x,Y,t,a,i,j,f,p,d[]{-9,-8,1,10,9,8,-1,-10};void C(){a=0;for(i=b[
+X]-3?0:8;i--;){for(j=1;b[Y=X+d[i]*j]==!t;)j++;if(1<j&&b[Y]==t)for(;j--;a++)f?b[
+X+d[i]*j]=t:0;}}int main(){for(;X--;p=b[X]=X%9&&X>8&&X<81?X-40&&X-50?X-41&&X-49
+?3:0:1:2);for(;p;x=f=0){for(X=7;X++<81;putchar("0@\n-"[b[X]]))C(),a?x=X:0;x?Y=0
+,t?X=x:scanf_s("%d%d",&Y,&X),f=X+=Y*9,C(),t=a?p=2,t:!t:(puts("PASS"),p--);t=!t;
+}}
